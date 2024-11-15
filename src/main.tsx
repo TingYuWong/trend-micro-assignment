68598c5c5fb54d38eefd52b58b1dde17e64a91bf
tina <sandy20615@gmail.com>
2024-11-15 15:19:33 +0800

    chore: initial commit

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+/// <reference types="vite-plugin-svgr/client" />
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
